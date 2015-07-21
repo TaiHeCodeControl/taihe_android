ada563500206837dc777086e009c2f744eb166cd
wangshaobin <1039067802@qq.com>
2015-07-21 17:14:11 +0800

    删除

--- app/src/main/java/com/taihe/eggshell/base/Constants.java
+++ app/src/main/java/com/taihe/eggshell/base/Constants.java
@@ -1,23 +1,9 @@
-﻿package com.taihe.eggshell.base;
-
-import android.os.Environment;
+package com.taihe.eggshell.base;
 
 /**
- * Created by tiahe on 2015/7/20.
+ * Created by Thinkpad on 2015/7/21.
  */
 public class Constants {
 
-    /**
-     * 升级包缓存的名称
-     */
-    public static final String UPDATE_PACKAGE_NAME = "eggshel_upgrade.apk";
-    public static final String APK_NAME = "xyb.apk";
-    public static final int PAY_TYPE_CASH_DISCOUNT = 1;
-
-    /**
-     * 升级的下载路径
-     */
-    public static final String UPGRADE_DOWNLOAD_PATH = Environment.getExternalStorageDirectory() + "/"
-            + "eggshell/download/";
-
+    public static final String APK_NAME = "eggshell";
 }
