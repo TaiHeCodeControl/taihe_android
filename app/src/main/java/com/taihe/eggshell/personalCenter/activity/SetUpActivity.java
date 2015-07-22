4ccf0a42c14e5b12b1447466ef6ab99cb701f961
mahuanh <488238623@qq.com>
2015-07-22 18:17:47 +0800

    退出当前账号

--- app/src/main/java/com/taihe/eggshell/personalCenter/activity/SetUpActivity.java
+++ app/src/main/java/com/taihe/eggshell/personalCenter/activity/SetUpActivity.java
@@ -8,6 +8,10 @@ import android.widget.RelativeLayout;
 
 import com.taihe.eggshell.R;
 import com.taihe.eggshell.base.BaseActivity;
+import com.taihe.eggshell.base.EggshellApplication;
+import com.taihe.eggshell.base.utils.ToastUtils;
+import com.taihe.eggshell.login.LoginActivity;
+import com.taihe.eggshell.widget.ChoiceDialog;
 
 /**
  * Created by Thinkpad on 2015/7/14.
@@ -20,11 +24,14 @@ public class SetUpActivity extends BaseActivity{
     private String url;
     private int updateCode;
 
+    private ChoiceDialog dialog;
+
     private RelativeLayout aboutLayout,helpLayout,feedBackLayout,updateLayout,quiteLayout;
     @Override
     public void initView() {
         setContentView(R.layout.activity_system_setup);
         super.initView();
+        overridePendingTransition(R.anim.activity_right_to_center, R.anim.activity_center_to_left);
 
         mContext = this;
         aboutLayout = (RelativeLayout)findViewById(R.id.id_to_about);
@@ -32,6 +39,28 @@ public class SetUpActivity extends BaseActivity{
         feedBackLayout = (RelativeLayout)findViewById(R.id.id_to_feedback);
         updateLayout = (RelativeLayout)findViewById(R.id.id_to_update);
         quiteLayout = (RelativeLayout)findViewById(R.id.id_to_quite);
+
+        dialog = new ChoiceDialog(mContext,new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                ToastUtils.show(mContext, "取消");
+            }
+        },new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog.dismiss();
+
+                EggshellApplication.getApplication().setUser(null);
+                Intent intent = new Intent(SetUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        dialog.getTitleText().setText("确定退出当前账号吗？");
+        dialog.getLeftButton().setText("以后再说");
+        dialog.getRightButton().setText("确认退出");
     }
 
     @Override
@@ -68,7 +97,7 @@ public class SetUpActivity extends BaseActivity{
                 checkUpdate();
                 break;
             case R.id.id_to_quite:
-
+               dialog.show();
                 break;
         }
     }
