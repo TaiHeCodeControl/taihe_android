df70c14a1545acf0803c8ab3596b4856c1921153
mahuanh <488238623@qq.com>
2015-07-24 16:22:18 +0800

    编辑个人简历

--- app/src/main/java/com/taihe/eggshell/personalCenter/activity/MyResumeEditActivity.java
+++ app/src/main/java/com/taihe/eggshell/personalCenter/activity/MyResumeEditActivity.java
@@ -1,9 +1,22 @@
 package com.taihe.eggshell.personalCenter.activity;
 
+import com.taihe.eggshell.R;
 import com.taihe.eggshell.base.BaseActivity;
 
 /**
  * Created by huan on 2015/7/24.
  */
 public class MyResumeEditActivity extends BaseActivity{
+
+    @Override
+    public void initView() {
+        setContentView(R.layout.activity_myresume_edit);
+        super.initView();
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        initTitle("编辑个人简历");
+    }
 }
