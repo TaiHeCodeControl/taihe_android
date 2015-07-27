c41b567cb781c72ef01ad1af3f8b21617ecff164
mahuanh <488238623@qq.com>
2015-07-27 18:26:40 +0800

    登录接口

--- app/src/main/java/com/taihe/eggshell/login/LoginActivity.java
+++ app/src/main/java/com/taihe/eggshell/login/LoginActivity.java
@@ -1,13 +1,24 @@
 package com.taihe.eggshell.login;
 
+import android.content.Context;
 import android.content.Intent;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.chinaway.framework.swordfish.network.http.Response;
+import com.chinaway.framework.swordfish.network.http.VolleyError;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.RequestParams;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
 import com.taihe.eggshell.R;
 import com.taihe.eggshell.base.BaseActivity;
+import com.taihe.eggshell.base.Urls;
 import com.taihe.eggshell.base.utils.FormatUtils;
 
 import android.widget.ImageView;
@@ -17,15 +28,25 @@ import com.taihe.eggshell.R;
 import com.taihe.eggshell.base.BaseActivity;
 import com.taihe.eggshell.base.EggshellApplication;
 import com.taihe.eggshell.base.utils.PrefUtils;
+import com.taihe.eggshell.base.utils.RequestUtils;
 import com.taihe.eggshell.base.utils.ToastUtils;
 import com.taihe.eggshell.main.MainActivity;
 import com.taihe.eggshell.main.entity.User;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  */
 public class LoginActivity extends BaseActivity {
 
+    private static final String TAG = "LoginActivity";
+
+    private Context mContext;
     private EditText et_userphone;
     private EditText et_password;
     private Button btn_login;
@@ -43,7 +64,7 @@ public class LoginActivity extends BaseActivity {
         setContentView(R.layout.activity_login);
         super.initView();
         overridePendingTransition(R.anim.activity_right_to_center, R.anim.activity_center_to_left);
-
+        mContext = this;
         et_userphone = (EditText) findViewById(R.id.et_login_userphone);
         et_password = (EditText) findViewById(R.id.et_login_password);
         btn_login = (Button) findViewById(R.id.btn_login_login);
@@ -99,14 +120,83 @@ public class LoginActivity extends BaseActivity {
             return;
         }
 
+//        HttpLogin();
+        loginFromNet();
         //保存用户登录信息
-//        EggshellApplication.getApplication().setUser(new User(1,"hh","18810309233"));
-//        User user = EggshellApplication.getApplication().getUser();
-//        System.out.println(user + "save=====================");
-        PrefUtils.saveStringPreferences(getApplicationContext(), PrefUtils.CONFIG, PrefUtils.KEY_USER_JSON, "{'id':1,'name':'xx','phoneNumber':'89898'}");
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
-        LoginActivity.this.finish();
 
+//        PrefUtils.saveStringPreferences(getApplicationContext(), PrefUtils.CONFIG, PrefUtils.KEY_USER_JSON, "{'id':1,'name':'xx','phoneNumber':'89898'}");
+//        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//        startActivity(intent);
+//        LoginActivity.this.finish();
+
+    }
+
+    private void loginFromNet() {
+
+        //返回监听事件
+        Response.Listener listener = new Response.Listener() {
+            @Override
+            public void onResponse(Object obj) {//返回值
+                try {
+                    Log.v(TAG, (String) obj);
+                    JSONObject jsonObject = new JSONObject((String) obj);
+                    int code = jsonObject.getInt("code");
+                    System.out.println("code=========" + code);
+                    if (code == 0) {
+                        ToastUtils.show(mContext, "登录成功");
+                        String data = jsonObject.getString("data");
+
+                        PrefUtils.saveStringPreferences(getApplicationContext(), PrefUtils.CONFIG, PrefUtils.KEY_USER_JSON, "{'id':1,'name':'xx','phoneNumber':'89898'}");
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        LoginActivity.this.finish();
+                    } else {
+                        String msg = jsonObject.getString("msg");
+                        ToastUtils.show(mContext, msg);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {//返回值
+//                    String err = new String(volleyError.networkResponse.data);
+//                    volleyError.networkResponse.statusCode;
+            }
+        };
+        String method = "&username=" + userphone + "&pw=" + password;
+        RequestUtils.createRequest_GET(mContext, Urls.getMopHostUrl(), method, false, "", "", listener, errorListener);
+    }
+
+
+    private void HttpLogin() {
+
+        RequestParams params = new RequestParams();
+        params.addQueryStringParameter("username", userphone);
+        params.addQueryStringParameter("pw", password);
+//        params.addBodyParameter("username", userphone);
+//        params.addBodyParameter("pw", password);
+
+//        String url = "http://195.198.1.195/index.php?m=api";
+        final String str = "&username=" + userphone + "&pw=" + password;
+        HttpUtils http = new HttpUtils();
+        http.send(HttpRequest.HttpMethod.GET, Urls.getMopHostUrl(), params, new RequestCallBack<String>() {
+
+            @Override
+            public void onSuccess(ResponseInfo<String> responseInfo) {
+                System.out.println(Urls.getMopHostUrl() + str);
+                String result = responseInfo.result;
+                System.out.println("LoginResult========= " + result);
+            }
+
+            @Override
+            public void onFailure(HttpException e, String s) {
+                System.out.println("请求服务器失败");
+            }
+        });
     }
 }
