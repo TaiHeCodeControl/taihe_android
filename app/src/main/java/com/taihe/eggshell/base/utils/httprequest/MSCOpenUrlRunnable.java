59ca104693affb5729a4584b9559f2403ce987c6
qq313915186 <qia7@qia7.com>
2015-08-17 18:01:24 +0800

    Revert "视频列表，玩出范"
    This reverts commit 1cc4c91b4398c39ad93223b7eccfabbc08b60430.

--- app/src/main/java/com/taihe/eggshell/base/utils/httprequest/MSCOpenUrlRunnable.java
+++ app/src/main/java/com/taihe/eggshell/base/utils/httprequest/MSCOpenUrlRunnable.java
@@ -1,4 +1,5 @@
 package com.taihe.eggshell.base.utils.httprequest;
+
 import java.io.InputStream;
 import java.util.List;
 
@@ -18,80 +19,80 @@ import android.os.Message;
 public abstract class MSCOpenUrlRunnable extends Handler implements Runnable {
 
 
-    public void onControl(MSCJSONObject jsonObject) throws JSONException {
+	public void onControl(MSCJSONObject jsonObject) throws JSONException {
 
-        String errcode;
+		String errcode;
 
-        if ((errcode = jsonObject.optString("errcode")).length() == 4) {
+		if ((errcode = jsonObject.optString("errcode")).length() == 4) {
 //			MSCTool.NowActivity.viewTool.getResult(errcode);
-        }
+		}
 
-    }
+	}
 
 
-    public void onControl(Bitmap obj) {
+	public void onControl(Bitmap obj) {
 
-    }
+	}
 
-    /** 访问失败调用的方法 **/
-    public void onStop(Exception e) {
-        if (e == null) {
-            e = new Exception("null:e");
-        }
+	/** 访问失败调用的方法 **/
+	public void onStop(Exception e) {
+		if (e == null) {
+			e = new Exception("null:e");
+		}
 //		MSCTool.NowActivity.toast("onStop:" + e.getMessage());
-    }
+	}
 
-    private MSCUrlManager url;
-    private int type;
+	private MSCUrlManager url;
+	private int type;
 
-    /***
-     * @param url
-     *            网址链接<br>
-     * @param
-     *
-     * @see
-     * **/
-    public MSCOpenUrlRunnable(MSCUrlManager url, int type) {
-        this.url = url;
-        this.type = type;
-    }
+	/***
+	 * @param url
+	 *            网址链接<br>
+	 * @param
+	 *
+	 * @see
+	 * **/
+	public MSCOpenUrlRunnable(MSCUrlManager url, int type) {
+		this.url = url;
+		this.type = type;
+	}
 
-    public static final int 图片类型 = 123;
-    public static final int post类型 = 111;
+	public static final int 图片类型 = 123;
+	public static final int post类型 = 111;
 
-    /***
-     * @param url
-     *            网址链接<br>
-     *
-     * **/
-    public MSCOpenUrlRunnable(MSCUrlManager url) {
-        System.out.println(url.toString());
-        this.url = url;
-        this.type = 0;
+	/***
+	 * @param url
+	 *            网址链接<br>
+	 * 
+	 * **/
+	public MSCOpenUrlRunnable(MSCUrlManager url) {
+		System.out.println(url.toString());
+		this.url = url;
+		this.type = 0;
+		
+		//this.urlParams = getloginmode(urlParams);
+	}
 
-        //this.urlParams = getloginmode(urlParams);
-    }
+	public List<MSCPostUrlParam> urlParams;
 
-    public List<MSCPostUrlParam> urlParams;
-
-    /***
-     * @param url
-     *            网址链接<br>
-     * @param
-     *
-     * @see
-     *
-     *
-     * **/
-    public MSCOpenUrlRunnable(MSCUrlManager url, List<MSCPostUrlParam> urlParams) {
-        this.url = url;
-        this.type = post类型;
-        this.urlParams = getloginmode(urlParams);
-        System.out.println(url.toString());
-    }
+	/***
+	 * @param url
+	 *            网址链接<br>
+	 * @param
+	 * 
+	 * @see
+	 * 
+	 * 
+	 * **/
+	public MSCOpenUrlRunnable(MSCUrlManager url, List<MSCPostUrlParam> urlParams) {
+		this.url = url;
+		this.type = post类型;
+		this.urlParams = getloginmode(urlParams);
+		System.out.println(url.toString());
+	}
 
 
-    private List<MSCPostUrlParam> getloginmode(List<MSCPostUrlParam> truemode) {
+	private List<MSCPostUrlParam> getloginmode(List<MSCPostUrlParam> truemode) {
 
 //		if (MSCTool.user == null || MSCTool.user.token == null) {
 //			MSCTool.user = new User();
@@ -102,12 +103,12 @@ public abstract class MSCOpenUrlRunnable extends Handler implements Runnable {
 //			truemode.add(token);
 //		}
 
-        return truemode;
+		return truemode;
 
-    }
+	}
 
-    @Override
-    public void run() {
+	@Override
+	public void run() {
 //		if (MSCTool.NowActivity.viewTool.GetWebType() == 0) {
 //			Dialog_tool.duanwang(MSCTool.NowActivity, this);
 //			return;
@@ -115,77 +116,77 @@ public abstract class MSCOpenUrlRunnable extends Handler implements Runnable {
 //
 //		MSCTool.ShowDialog();
 
-        Message msg = new Message();
-        switch (type) {
+		Message msg = new Message();
+		switch (type) {
 
-            // Post专用连接
-            case post类型:
-                try {
-                    msg.obj = MSCUrlManager.NewopenpostUrlreString(url.GetUrl(),
-                            urlParams);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
+		// Post专用连接
+		case post类型:
+			try {
+				msg.obj = MSCUrlManager.NewopenpostUrlreString(url.GetUrl(),
+						urlParams);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			break;
 
-            case 图片类型:
+		case 图片类型:
 
-                try {
-                    msg.obj = getfileforbitmap(url);
-                } catch (Exception e1) {
-                    e1.printStackTrace();
-                }
-                break;
-            default:
-                JSONObject jsonobject = new JSONObject();
-                try {
-                    jsonobject = new JSONObject(MSCUrlManager.openUrlreString(url
-                            .GetUrl()));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                msg.obj = jsonobject;
-                break;
-        }
-        sendMessage(msg);
+			try {
+				msg.obj = getfileforbitmap(url);
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+			break;
+		default:
+			JSONObject jsonobject = new JSONObject();
+			try {
+				jsonobject = new JSONObject(MSCUrlManager.openUrlreString(url
+						.GetUrl()));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			msg.obj = jsonobject;
+			break;
+		}
+		sendMessage(msg);
 //		MSCTool.DismissDialog();
-    }
+	}
 
-    private Bitmap getfileforbitmap(MSCUrlManager imgurl) throws Exception {
-        InputStream sre = imgurl.GetUrl().openStream();
-        Bitmap bitmap = BitmapFactory.decodeStream(sre);
-        sre.close();
-        return bitmap;
+	private Bitmap getfileforbitmap(MSCUrlManager imgurl) throws Exception {
+		InputStream sre = imgurl.GetUrl().openStream();
+		Bitmap bitmap = BitmapFactory.decodeStream(sre);
+		sre.close();
+		return bitmap;
 
-    }
+	}
 
-    public void chongshi() {
-        run();
-    }
+	public void chongshi() {
+		run();
+	}
 
-    @Override
-    public void handleMessage(Message msg) {
-        switch (type) {
-            case 图片类型:
+	@Override
+	public void handleMessage(Message msg) {
+		switch (type) {
+		case 图片类型:
 
-                onControl((Bitmap) msg.obj);
+			onControl((Bitmap) msg.obj);
 
-                break;
+			break;
 
-            default:
-                JSONObject jsonObject = (JSONObject) msg.obj;
+		default:
+			JSONObject jsonObject = (JSONObject) msg.obj;
 
-                try {
-                    onControl(new MSCJSONObject(jsonObject));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    onStop(e);
-                }
+			try {
+				onControl(new MSCJSONObject(jsonObject));
+			} catch (Exception e) {
+				e.printStackTrace();
+				onStop(e);
+			}
 
-                break;
-        }
+			break;
+		}
 
-        super.handleMessage(msg);
-    }
+		super.handleMessage(msg);
+	}
 
 }
--- app/src/main/java/com/taihe/eggshell/meetinginfo/InfoDetailActivity.java
+++ app/src/main/java/com/taihe/eggshell/meetinginfo/InfoDetailActivity.java
@@ -1,14 +1,10 @@
 package com.taihe.eggshell.meetinginfo;
 
 import android.content.Context;
-import android.content.Intent;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.taihe.eggshell.R;
 import com.taihe.eggshell.base.BaseActivity;
-
-import net.tsz.afinal.FinalBitmap;
 
 /**
  * Created by wang on 2015/8/12.
@@ -19,7 +15,7 @@ public class InfoDetailActivity extends BaseActivity{
     private Context mContext;
 
     private TextView mainPlat,startTime,address,telPhone,callPerson,comeWay,jobBrief;
-    private ImageView imgLog;
+
     @Override
     public void initView() {
         setContentView(R.layout.activity_info_detail);
@@ -32,23 +28,15 @@ public class InfoDetailActivity extends BaseActivity{
         callPerson = (TextView)findViewById(R.id.id_person);
         comeWay = (TextView)findViewById(R.id.id_info_way);
         jobBrief = (TextView)findViewById(R.id.id_company_brief);
-        imgLog = (ImageView)findViewById(R.id.id_info_logo);
 
     }
 
     @Override
     public void initData() {
         super.initData();
-        Intent intent = getIntent();
+
         initTitle("详情");
-        mainPlat.setText(intent.getStringExtra("title"));
-        startTime.setText(intent.getStringExtra("starttime") + "至" + intent.getStringExtra("endtime"));
-        address.setText(intent.getStringExtra("address"));
-        telPhone.setText(intent.getStringExtra("telphone"));
-        callPerson.setText(intent.getStringExtra("user"));
-        comeWay.setText(intent.getStringExtra("traffic_route"));
-        jobBrief.setText(intent.getStringExtra("content"));
-        FinalBitmap bitmap = FinalBitmap.create(mContext);
-        bitmap.display(imgLog,intent.getStringExtra("logo"));
+
+
     }
 }
