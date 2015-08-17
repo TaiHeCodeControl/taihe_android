9735519742d17413f45f8728061de8d5c29e272a
mahuanh <488238623@qq.com>
2015-08-17 11:55:18 +0800

    行业类别。月薪范围。。。

--- app/src/main/java/com/taihe/eggshell/job/activity/IndustryActivity.java
+++ app/src/main/java/com/taihe/eggshell/job/activity/IndustryActivity.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import com.taihe.eggshell.R;
@@ -26,8 +27,18 @@ public class IndustryActivity extends BaseActivity {
     private TextView tv_select;
     private Intent intent;
     private String filterString , title,selectString;
+
     public static final int RESULT_CODE_INDUSTRYTYPE = 10;
     public static final int RESULT_CODE_JOBYEAR = 11;
+    public static final int RESULT_CODE_EDU = 12;
+    public static final int RESULT_CODE_POSITION = 13;
+    public static final int RESULT_CODE_JOBCITY = 14;
+    public static final int RESULT_CODE_SALARY = 15;
+    public static final int RESULT_CODE_JOBTYPE = 16;
+    public static final int RESULT_CODE_PUBTIME = 17;
+
+    private int RESULT_CODE = 0;
+
     @Override
     public void initView() {
         setContentView(R.layout.activity_industry_list);
@@ -35,26 +46,10 @@ public class IndustryActivity extends BaseActivity {
 
         industLists = new ArrayList<String>();
 
-
-
         intent = getIntent();
         filterString = intent.getStringExtra("Filter");
-        if(filterString.equals("industry")){
-            title = "行业类别";
-            selectString = "请选择行业类别";
 
-            for (int i = 0; i < 10; i++) {
-                industLists.add("计算机/互联网"+i);
-            }
-        }else   if(filterString.equals("jobyears")){
-            title = "工作经验";
-            selectString = "请选择工作经验";
-
-            for (int i = 0; i < 10; i++) {
-                industLists.add(i+"年");
-            }
-        }
-
+        initListView();
 
         tv_select = (TextView) findViewById(R.id.tv_industry_select);
         tv_select.setText(selectString);
@@ -65,12 +60,9 @@ public class IndustryActivity extends BaseActivity {
 
                 Intent intent = new Intent();
                 intent.putExtra("data", industLists.get(position));
-                if(filterString.equals("industry")){
-                    setResult(RESULT_CODE_INDUSTRYTYPE,intent);
-                }
-                if(filterString.equals("jobyears")){
-                    setResult(RESULT_CODE_JOBYEAR,intent);
-                }
+
+                    setResult(RESULT_CODE,intent);
+
                 finish();
 
             }
@@ -79,6 +71,7 @@ public class IndustryActivity extends BaseActivity {
 
 
     }
+
 
     @Override
     public void initData() {
@@ -111,6 +104,81 @@ public class IndustryActivity extends BaseActivity {
 
             return view;
         }
+    }
+
+
+
+    private void initListView() {
+
+        if(filterString.equals("industry")){
+            title = "行业类别";
+            selectString = "请选择行业类别";
+
+            for (int i = 0; i < 10; i++) {
+                industLists.add("计算机/互联网"+i);
+            }
+
+            RESULT_CODE = RESULT_CODE_INDUSTRYTYPE;
+
+        }else   if(filterString.equals("jobyears")){
+            title = "工作经验";
+            selectString = "请选择工作经验";
+
+            for (int i = 0; i < 10; i++) {
+                industLists.add(i+"年");
+            }
+
+            RESULT_CODE = RESULT_CODE_JOBYEAR;
+        }else if(filterString.equals("position")){
+            title = "职位类别";
+            selectString = "请选择您的职位";
+
+            for(int i = 0; i < 10 ; i++){
+
+                industLists.add("工程师" + i);
+            }
+
+            RESULT_CODE = RESULT_CODE_POSITION;
+        }else if(filterString.equals("salary")){
+            title = "月薪范围";
+            selectString = "请选择您期望的月薪范围";
+
+            for(int i = 0; i < 10 ; i++){
+
+                industLists.add( i + "000-" + i + 1 + "999");
+            }
+
+            RESULT_CODE = RESULT_CODE_SALARY;
+        }else if(filterString.equals("pubtime")){
+            title = "发布时间";
+            selectString = "请选择职位发布时间";
+
+            for(int i = 0; i < 10 ; i++){
+
+                industLists.add( i + "周");
+            }
+            RESULT_CODE = RESULT_CODE_PUBTIME;
+        }else if(filterString.equals("edu")){
+            title = "学历要求";
+            selectString = "请选择学历要求";
+
+            for(int i = 0; i < 10 ; i++){
+
+                industLists.add(i + "本");
+            }
+
+            RESULT_CODE = RESULT_CODE_EDU;
+        }else if(filterString.equals("jobtype")){
+            title = "工作性质";
+            selectString = "请选择工作性质";
+
+            for(int i = 0; i < 10 ; i++){
+
+                industLists.add(i + "全职");
+            }
+            RESULT_CODE = RESULT_CODE_JOBTYPE;
+        }
+
     }
 
 }
