f85134e45df351f95db73588004477c517e8dd3c
qq313915186 <qia7@qia7.com>
2015-08-18 09:01:30 +0800

    Revert "Revert "Revert "视频列表，玩出范"""
    This reverts commit 4fd050e45da1871754c3f1b42f48e0fe7ac916ef.

--- app/src/main/java/com/taihe/eggshell/main/InternshipFragment.java
+++ app/src/main/java/com/taihe/eggshell/main/InternshipFragment.java
@@ -1,21 +1,17 @@
 package com.taihe.eggshell.main;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.Display;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.ScrollView;
 import android.widget.TextView;
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshGridView;
 
 import com.taihe.eggshell.R;
 import com.taihe.eggshell.base.utils.httprequest.MSCJSONObject;
@@ -25,7 +21,6 @@ import com.taihe.eggshell.base.utils.httprequest.MSCUrlManager;
 import com.taihe.eggshell.main.adapter.VideoAdapterHead;
 import com.taihe.eggshell.videoplay.mode.VideoInfoMode;
 import com.taihe.eggshell.main.adapter.VideoAdapterGride;
-import com.taihe.eggshell.widget.MyGridView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -35,35 +30,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InternshipFragment extends Fragment implements View.OnClickListener{
-    private LinearLayout lin_back,lin_head_video,id_lin_more;
+    private LinearLayout lin_back,lin_head_video;
     private ImageView id_back;
     private TextView id_title;
-    private MyGridView videoGrideView;
+    private PullToRefreshGridView videoGrideView;
     private VideoAdapterGride videoAdapter;
     private  GridView grid_head_video;
-    private int viewwidth1 = 0,index=0;
-    private ScrollView scroll_hotnotes;
-    List<VideoInfoMode> listInfo;
-    int pagesize=4,page=1;
-    private ProgressBar progressBar;
-    Handler mHandler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-        }
-    };
+
 	@Override
 	public View onCreateView(LayoutInflater inflater , ViewGroup container , Bundle savedInstanceState){
 		View v = inflater.inflate(R.layout.fragment_intertship, null) ;
         lin_back = (LinearLayout)v.findViewById(R.id.lin_back);
         lin_head_video = (LinearLayout)v.findViewById(R.id.lin_head_video);
         id_title = (TextView) v.findViewById(R.id.id_title);
-        id_lin_more = (LinearLayout) v.findViewById(R.id.id_lin_more);
         grid_head_video = (GridView) v.findViewById(R.id.grid_head_video);
-        videoGrideView = (MyGridView) v.findViewById(R.id.id_video_grideview);
-        scroll_hotnotes = (ScrollView) v.findViewById(R.id.scroll_hotnotes);
-        progressBar = (ProgressBar) v.findViewById(R.id.loadingmore);
-        id_lin_more.setVisibility(View.GONE);
+
+        videoGrideView = (PullToRefreshGridView) v.findViewById(R.id.id_video_grideview);
         init();
         initData();
         return v ;
@@ -71,47 +53,23 @@ public class InternshipFragment extends Fragment implements View.OnClickListener
     void init(){
         id_title.setText("公开课");
         lin_back.setVisibility(View.GONE);
+        videoGrideView.setMode(PullToRefreshBase.Mode.BOTH);
+        final List<VideoInfoMode> vList = new ArrayList<VideoInfoMode>();
+
         videoAdapter = new VideoAdapterGride(getActivity());
-        listInfo = new ArrayList<VideoInfoMode>();
 
-        Display dw = getActivity().getWindowManager().getDefaultDisplay();
-        viewwidth1 = dw.getWidth();
-
-        // 滑动加载
-        scroll_hotnotes.setOnTouchListener(new View.OnTouchListener() {
+        videoGrideView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<GridView>() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                // TODO Auto-generated method stub
+            public void onPullDownToRefresh(PullToRefreshBase<GridView> refreshView) {
+//                videoAdapter.setVideoData(vList);
+                initData();
+                videoGrideView.onRefreshComplete();
+            }
 
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        System.out.print("ACTION_DOWN:"+index);
-                        Log.e("err","ACTION_DOWN:"+index);
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        index++;
-                        Log.e("err1","ACTION_DOWN:"+index);
-                        break;
-                    default:
-                        break;
-                }
-                if (event.getAction() == MotionEvent.ACTION_UP && index > 0) {
-                    index = 0;
-                    View view = ((ScrollView) v).getChildAt(0);
-                    if (view.getMeasuredHeight() <= v.getScrollY()
-                            + v.getHeight()) {
-                        // 加载数据代码
-                        if (listInfo.size() <= 3) {
-                            listInfo = new ArrayList<VideoInfoMode>();
-                            page = 1;
-                        } else {
-                            page++;
-                        }
-                        Log.e("err","=====:"+page);
-                        updataUI();
-                    }
-                }
-                return false;
+            @Override
+            public void onPullUpToRefresh(PullToRefreshBase<GridView> refreshView) {
+//                videoAdapter.setVideoData(vList);
+                videoGrideView.onRefreshComplete();
             }
         });
     }
@@ -129,46 +87,42 @@ public class InternshipFragment extends Fragment implements View.OnClickListener
        headAdapter.setVideoData(listHead);
        grid_head_video.setAdapter(headAdapter);
 
-       updataUI();
-   }
-    public void updataUI(){
-        MSCUrlManager url = new MSCUrlManager("video","getPageList",pagesize,page);
-        List<MSCPostUrlParam> list = new ArrayList<MSCPostUrlParam>();
-        list.add(new MSCPostUrlParam("token", pagesize));
-//        list.add(new MSCPostUrlParam("pagesize", pagesize));
-//        list.add(new MSCPostUrlParam("page", page));
-        new Thread(new MSCOpenUrlRunnable(url, list) {
-            @Override
-            public void onControl(MSCJSONObject jsonObject) throws JSONException {
-                if (jsonObject.optString("code").equals("0")) {
-                    JSONArray j1 = jsonObject.getJSONArray("data");
-                    JSONObject j2;
-                    for(int i=0;i<j1.length();i++){
-                        VideoInfoMode vMode = new VideoInfoMode();
-                        j2 = j1.getJSONObject(i);
-                        vMode.setId(j2.optString("id").toString());
-                        vMode.setC_id(j2.optString("c_id").toString());
-                        vMode.setVideo_id(j2.optString("video_id").toString());
-                        vMode.setVideo_name(j2.optString("video_name").toString());
-                        vMode.setVideo_hour(j2.optString("video_hour").toString());
-                        vMode.setVideo_teacher(j2.optString("video_teacher").toString());
-                        vMode.setVideo_about(j2.optString("video_about").toString());
-                        vMode.setVideo_obvious(j2.optString("video_obvious").toString());
-                        vMode.setStatus(j2.optString("status").toString());
-                        vMode.setVimage(j2.optString("vimage").toString());
-                        listInfo.add(vMode);
-                    }
-                    videoAdapter.setVideoData(listInfo);
-                    videoGrideView.setAdapter(videoAdapter);
-//                    id_lin_more.setVisibility(View.INVISIBLE);
-//                    videoAdapter.notifyDataSetChanged();
-                } else {
+       MSCUrlManager url = new MSCUrlManager("video","getListImage");
+       List<MSCPostUrlParam> list = new ArrayList<MSCPostUrlParam>();
+       list.add(new MSCPostUrlParam("token", "pagenum"));
+       new Thread(new MSCOpenUrlRunnable(url, list) {
+           @Override
+           public void onControl(MSCJSONObject jsonObject) throws JSONException {
+               if (jsonObject.optString("code").equals("0")) {
+                   JSONArray j1 = jsonObject.getJSONArray("data");
+                   JSONObject j2;
+                   List<VideoInfoMode> list = new ArrayList<VideoInfoMode>();
+                   for(int i=0;i<j1.length();i++){
+                       VideoInfoMode vMode = new VideoInfoMode();
+                       j2 = j1.getJSONObject(i);
+                       vMode.setId(j2.optString("id").toString());
+                       vMode.setC_id(j2.optString("c_id").toString());
+                       vMode.setVideo_id(j2.optString("video_id").toString());
+                       vMode.setVideo_name(j2.optString("video_name").toString());
+                       vMode.setVideo_hour(j2.optString("video_hour").toString());
+                       vMode.setVideo_teacher(j2.optString("video_teacher").toString());
+                       vMode.setVideo_about(j2.optString("video_about").toString());
+                       vMode.setVideo_obvious(j2.optString("video_obvious").toString());
+                       vMode.setStatus(j2.optString("status").toString());
+                       vMode.setVimage(j2.optString("vimage").toString());
+                       list.add(vMode);
+                   }
+                   videoAdapter.setVideoData(list);
+                   videoGrideView.setAdapter(videoAdapter);
+                   videoAdapter.notifyDataSetChanged();
+                   videoGrideView.onRefreshComplete();
+               } else {
 //					Toast.makeText(getActivity(), jsonObject.length()+"false", Toast.LENGTH_LONG).show();
-                }
-                super.onControl(jsonObject);
-            }
-        }).start();
-    }
+               }
+               super.onControl(jsonObject);
+           }
+       }).start();
+   }
 
     @Override
     public void onClick(View view) {
@@ -176,5 +130,4 @@ public class InternshipFragment extends Fragment implements View.OnClickListener
 
         }
     }
-
 }
--- app/src/main/java/com/taihe/eggshell/main/NearbyFragment.java
+++ app/src/main/java/com/taihe/eggshell/main/NearbyFragment.java
@@ -1,135 +1,45 @@
 package com.taihe.eggshell.main;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 
-import com.chinaway.framework.swordfish.network.http.Response;
-import com.chinaway.framework.swordfish.network.http.VolleyError;
-import com.handmark.pulltorefresh.library.PullToRefreshBase;
-import com.handmark.pulltorefresh.library.PullToRefreshGridView;
-import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
 import com.taihe.eggshell.R;
-import com.taihe.eggshell.base.utils.RequestUtils;
-import com.taihe.eggshell.base.utils.httprequest.MSCJSONObject;
-import com.taihe.eggshell.base.utils.httprequest.MSCOpenUrlRunnable;
-import com.taihe.eggshell.base.utils.httprequest.MSCPostUrlParam;
-import com.taihe.eggshell.base.utils.httprequest.MSCUrlManager;
-import com.taihe.eggshell.main.adapter.PlayAdapter;
-import com.taihe.eggshell.main.mode.PlayInfoMode;
-import com.taihe.eggshell.videoplay.mode.VideoInfoMode;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.taihe.eggshell.map.LocationMapActivity;
 
 public class NearbyFragment extends Fragment implements View.OnClickListener{
     private TextView id_title;
     private LinearLayout lin_back;
-    private Context context;
-    private PullToRefreshGridView playView;
-    private PlayAdapter playAdapter;
-    int limit=2,page=1;
-    List<PlayInfoMode> list;
-    Handler mHandler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch (msg.what){
-                case 100:
-                    try{
-                        playView.isSelected();
-                        JSONArray j1 = new JSONArray(msg.obj.toString());
-                        JSONObject j2;
-                        for(int i=0;i<j1.length();i++){
-                            PlayInfoMode vMode = new PlayInfoMode();
-                            j2 = j1.getJSONObject(i);
-                            vMode.setId(j2.optString("id").toString());
-                            vMode.setTitle(j2.optString("title").toString());
-                            vMode.setAddress(j2.optString("address").toString());
-                            vMode.setOrganizers(j2.optString("organizers").toString());
-                            vMode.setEvery_time(j2.optString("every_time").toString());
-                            vMode.setUser(j2.optString("user").toString());
-                            vMode.setTelphone(j2.optString("telphone").toString());
-                            vMode.setTraffic_route(j2.optString("traffic_route").toString());
-                            vMode.setLogo(j2.optString("logo").toString());
-                            vMode.setContent(j2.optString("content").toString());
-                            vMode.setStarttime(j2.optString("starttime").toString());
-                            vMode.setEndtime(j2.optString("endtime").toString());
-                            list.add(vMode);
-                        }
-                        playAdapter.setPlayData(list);
-                        playView.setAdapter(playAdapter);
-                    }catch (Exception ex){
-                        ex.printStackTrace();
-                    }
-                    break;
-            }
-        }
-    };
+
 	@Override
 	public View onCreateView(LayoutInflater inflater , ViewGroup container , Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_around, null) ;
         lin_back = (LinearLayout)v.findViewById(R.id.lin_back);
         id_title = (TextView) v.findViewById(R.id.id_title);
-        playView = (PullToRefreshGridView) v.findViewById(R.id.id_video_listview);
-        init();
-        initData();
 		return v;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		init();
 	}
 
 	public void init(){
         id_title.setText("玩出范");
         lin_back.setVisibility(View.GONE);
-        playView.setMode(PullToRefreshBase.Mode.BOTH);
-        playAdapter = new PlayAdapter(getActivity());
-        list = new ArrayList<PlayInfoMode>();
-        playView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<GridView>() {
-            @Override
-            public void onPullDownToRefresh(PullToRefreshBase<GridView> refreshView) {
-                page=1;
-                list.clear();
-                getListData();
-                playView.onRefreshComplete();
-            }
-
-            @Override
-            public void onPullUpToRefresh(PullToRefreshBase<GridView> refreshView) {
-                page++;
-                getListData();
-                playView.onRefreshComplete();
-            }
-        });
 	}
-
-    public void initData(){
-//        List<PlayInfoMode> list = new ArrayList<PlayInfoMode>();
-//        for(int i=0;i<10;i++) {
-//            PlayInfoMode vMode = new PlayInfoMode();
-//            vMode.setTitle("哈哈"+(i+1));
-//            list.add(vMode);
-//        }
-//        playAdapter.setPlayData(list);
-//        playView.setAdapter(playAdapter);
-//        playView.onRefreshComplete();
-
-        getListData();
-    }
 
 	@Override
 	public void onClick(View view) {
@@ -137,51 +47,4 @@ public class NearbyFragment extends Fragment implements View.OnClickListener{
 
 		}
 	}
-
-    private void getListData() {
-
-        //返回监听事件
-        Response.Listener listener = new Response.Listener() {
-            @Override
-            public void onResponse(Object obj) {//返回值
-                try {
-                    JSONObject jsonObject = new JSONObject((String) obj);
-                    int code = jsonObject.getInt("code");
-                    System.out.println("code=========" + code);
-                    if (code == 1) {
-                        String data = jsonObject.getString("data");
-                        Message msg = new Message();
-                        msg.what=100;
-                        msg.obj=data;
-                        mHandler.sendMessage(msg);
-                        Log.e("data",data);
-                    } else {
-                        String msg = jsonObject.getString("msg");
-//                        ToastUtils.show(mContext, msg);
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        Response.ErrorListener errorListener = new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {//返回值
-//                    String err = new String(volleyError.networkResponse.data);
-//                    volleyError.networkResponse.statusCode;
-            }
-        };
-
-        Map<String,String> map = new HashMap<String,String>();
-//        map.put("m","act");
-//        map.put("c","list");
-//        map.put("tokey","740c957a");
-        map.put("limit",""+limit);
-        map.put("page",""+page);
-
-        String url = "http://195.198.1.122:8066/eggker/phpyun/api/admin/index.php?m=act&c=list";
-        RequestUtils.createRequest(getActivity(), url, "", true, map, true, listener, errorListener);
-    }
-
 }
