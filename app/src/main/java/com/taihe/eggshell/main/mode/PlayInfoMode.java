69dca2f72f3bc570d2ead1c76a6c9a2578100a08
qq313915186 <qia7@qia7.com>
2015-08-18 09:22:42 +0800

    Revert "Revert "Revert "Revert "视频列表，玩出范""""
    This reverts commit f85134e45df351f95db73588004477c517e8dd3c.

---
+++ app/src/main/java/com/taihe/eggshell/main/mode/PlayInfoMode.java
@@ -0,0 +1,115 @@
+package com.taihe.eggshell.main.mode;
+
+/**
+ * Created by Administrator on 2015/8/14.
+ */
+public class PlayInfoMode {
+    public String id;
+    public String title;
+    public String address;
+    public String organizers;
+    public String every_time;
+    public String user;
+    public String telphone;
+    public String traffic_route;
+    public String logo;
+    public String content;
+    public String starttime;
+    public String endtime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getOrganizers() {
+        return organizers;
+    }
+
+    public void setOrganizers(String organizers) {
+        this.organizers = organizers;
+    }
+
+    public String getEvery_time() {
+        return every_time;
+    }
+
+    public void setEvery_time(String every_time) {
+        this.every_time = every_time;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getTelphone() {
+        return telphone;
+    }
+
+    public void setTelphone(String telphone) {
+        this.telphone = telphone;
+    }
+
+    public String getTraffic_route() {
+        return traffic_route;
+    }
+
+    public void setTraffic_route(String traffic_route) {
+        this.traffic_route = traffic_route;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getStarttime() {
+        return starttime;
+    }
+
+    public void setStarttime(String starttime) {
+        this.starttime = starttime;
+    }
+
+    public String getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(String endtime) {
+        this.endtime = endtime;
+    }
+}
