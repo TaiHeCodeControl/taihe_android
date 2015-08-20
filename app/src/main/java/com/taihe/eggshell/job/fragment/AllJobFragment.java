5c772d630de13720495ac6cae3b0d1ab44c155e6
mahuanh <488238623@qq.com>
2015-08-20 14:58:40 +0800

    职位列表界面修改

--- app/src/main/java/com/taihe/eggshell/job/fragment/AllJobFragment.java
+++ app/src/main/java/com/taihe/eggshell/job/fragment/AllJobFragment.java
@@ -3,16 +3,23 @@ package com.taihe.eggshell.job.fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshExpandableListView;
 import com.taihe.eggshell.R;
 import com.taihe.eggshell.widget.JobApplyDialogUtil;
 import com.taihe.eggshell.job.activity.JobDetailActivity;
@@ -26,7 +33,7 @@ import java.util.List;
  * 全部职位列表
  * Created by huan on 2015/8/6.
  */
-public class AllJobFragment extends Fragment implements View.OnClickListener{
+public class AllJobFragment extends Fragment implements View.OnClickListener {
 
 
     private AllJobAdapter adapter;
@@ -36,27 +43,28 @@ public class AllJobFragment extends Fragment implements View.OnClickListener{
     private List<JobInfo> jobInfos = null;
     private JobInfo jobInfo;
 
+    private View footerView;
     private ListView list_job_all;
     private View rootView;
     private Context mContext;
 
     @Override
-    public View onCreateView(LayoutInflater inflater , ViewGroup container , Bundle savedInstanceState){
-        rootView = inflater.inflate(R.layout.fragment_job_all, null) ;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_job_all, null);
 
         mContext = getActivity();
         initView();
         initDate();
-        return rootView ;
+        return rootView;
     }
 
 
-
     private void initView() {
+
         jobInfos = new ArrayList<JobInfo>();
 
-        for(int i = 0; i < 10;i++){
-            jobInfo = new JobInfo(false,i);
+        for (int i = 0; i < 10; i++) {
+            jobInfo = new JobInfo(false, i);
             jobInfos.add(jobInfo);
         }
         list_job_all = (ListView) rootView.findViewById(R.id.list_job_all);
@@ -71,22 +79,50 @@ public class AllJobFragment extends Fragment implements View.OnClickListener{
             }
         });
 
-
     }
 
 
     private void initDate() {
-        adapter = new AllJobAdapter(mContext,jobInfos,true);
-        View footerView = View.inflate(mContext,R.layout.list_job_all_footer,null);
-
-
+        adapter = new AllJobAdapter(mContext, jobInfos, true);
+        footerView = View.inflate(mContext, R.layout.list_job_all_footer, null);
+        footerView.setVisibility(View.GONE);
         list_job_all.addFooterView(footerView);
 
         list_job_all.setAdapter(adapter);
 
-        Button btn_shenqing = (Button)footerView.findViewById(R.id.btn_alljob_shenqing);
+        list_job_all.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView absListView, int scrollState) {
+                //当不滚动时
+                if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE) {
+                    //判断是否滚动到底部
+                    if (absListView.getLastVisiblePosition() == absListView.getCount() - 1) {
+                        //加载更多
+                        footerView.setVisibility(View.VISIBLE);
+
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                TextView tv = (TextView) footerView.findViewById(R.id.tv_alljob_footer_txt);
+                                ProgressBar pb = (ProgressBar) footerView.findViewById(R.id.pb_alljob_footer_loading);
+                                pb.setVisibility(View.GONE);
+                                tv.setText("没有更多了");
+                            }
+                        },2000);
+                    }
+                }
+            }
+
+            @Override
+            public void onScroll(AbsListView absListView, int i, int i1, int i2) {
+
+            }
+        });
+
+        Button btn_shenqing = (Button) rootView.findViewById(R.id.btn_alljob_shenqing);
         btn_shenqing.setOnClickListener(this);
-        cb_selectAll = (CheckBox) footerView.findViewById(R.id.cb_findjob_selectall);
+        cb_selectAll = (CheckBox) rootView.findViewById(R.id.cb_findjob_selectall);
 
         cb_selectAll.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
@@ -107,12 +143,9 @@ public class AllJobFragment extends Fragment implements View.OnClickListener{
     }
 
 
-
-
-
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btn_alljob_shenqing:
                 JobApplyDialogUtil.isApplyJob(mContext);
                 postJob();
@@ -121,7 +154,7 @@ public class AllJobFragment extends Fragment implements View.OnClickListener{
     }
 
     public void postJob() {
-        for(JobInfo jobInfo : jobInfos){
+        for (JobInfo jobInfo : jobInfos) {
 //            System.out.println(jobInfo.getId()+"======"+jobInfo.isChecked());
 
         }
--- app/src/main/java/com/taihe/eggshell/main/SplashActivity.java
+++ app/src/main/java/com/taihe/eggshell/main/SplashActivity.java
@@ -1,14 +1,23 @@
 package com.taihe.eggshell.main;
 
 import android.app.Activity;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.GeofenceClient;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.taihe.eggshell.R;
 import com.taihe.eggshell.base.utils.PrefUtils;
 import com.taihe.eggshell.main.GuideActivity;
@@ -20,6 +29,7 @@ import com.taihe.eggshell.main.MainActivity;
 public class SplashActivity extends Activity {
 
     private Context mContext;
+    private String Longitude = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +37,7 @@ public class SplashActivity extends Activity {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         mContext = this;
-        View view = View.inflate(mContext, R.layout.activity_splash,null);
+        View view = View.inflate(mContext, R.layout.activity_splash, null);
         setContentView(view);
 
         Animation animation = new AlphaAnimation(0.1f, 1.0f);
@@ -44,14 +54,14 @@ public class SplashActivity extends Activity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                boolean isFirstUsing = PrefUtils.getBooleanData(mContext,PrefUtils.KEY_FIRST_USE,true);
-                if(isFirstUsing){
-                    Intent intent = new Intent(mContext,GuideActivity.class);
+                boolean isFirstUsing = PrefUtils.getBooleanData(mContext, PrefUtils.KEY_FIRST_USE, true);
+                if (isFirstUsing) {
+                    Intent intent = new Intent(mContext, GuideActivity.class);
                     startActivity(intent);
                     finish();
-                }else{
-                    Intent intent = new Intent(mContext,MainActivity.class);
-                    intent.putExtra("MeFragment","");
+                } else {
+                    Intent intent = new Intent(mContext, MainActivity.class);
+                    intent.putExtra("MeFragment", "");
                     startActivity(intent);
                     finish();
                 }
@@ -60,6 +70,92 @@ public class SplashActivity extends Activity {
         });
 
         view.setAnimation(animation);
+
+        initLocation();
+
+
     }
 
+
+    //====================定位-----------==============
+    public LocationClient mLocationClient;
+    public GeofenceClient mGeofenceClient;
+    public MyLocationListener mMyLocationListener;
+
+
+    public Vibrator mVibrator;
+
+    private void initLocation() {
+        mLocationClient = new LocationClient(this.getApplicationContext());
+        mMyLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(mMyLocationListener);
+        mGeofenceClient = new GeofenceClient(getApplicationContext());
+
+
+        mVibrator =(Vibrator)getApplicationContext().getSystemService(Service.VIBRATOR_SERVICE);
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//设置定位模式
+        option.setCoorType("gcj02");//返回的定位结果是百度经纬度，默认值gcj02
+        int span=1000;
+        option.setScanSpan(span);//设置发起定位请求的间隔时间为5000ms
+        option.setIsNeedAddress(true);
+        mLocationClient.setLocOption(option);
+
+        if(TextUtils.isEmpty(Longitude)){
+            mLocationClient.start();
+
+        }else{
+            mLocationClient.stop();
+
+        }
+    }
+
+
+    /**
+     * 实现实位回调监听
+     */
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //Receive Location
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("time : ");
+            sb.append(location.getTime());
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());
+            if (location.getLocType() == BDLocation.TypeGpsLocation) {
+                sb.append("\nspeed : ");
+                sb.append(location.getSpeed());
+                sb.append("\nsatellite : ");
+                sb.append(location.getSatelliteNumber());
+                sb.append("\ndirection : ");
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                sb.append(location.getDirection());
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                //运营商信息
+                sb.append("\noperationers : ");
+                sb.append(location.getOperators());
+            }
+            Log.i("BaiduLocationApiDem", sb.toString());
+        }
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        // TODO Auto-generated method stub
+        mLocationClient.stop();
+        super.onStop();
+    }
 }
