454637604194bf6120ead6d300556549938799e0
mahuanh <488238623@qq.com>
2015-08-27 14:01:18 +0800

    xx

--- app/src/main/java/com/taihe/eggshell/widget/JobApplyDialogUtil.java
+++ app/src/main/java/com/taihe/eggshell/widget/JobApplyDialogUtil.java
@@ -17,18 +17,15 @@ public class JobApplyDialogUtil {
 
     private static MyDialog isApplyDialog = null;
 
-    public static void isApplyJob(final Context mContext,int selectNum,int postedNum){
-
-
+    public static void isApplyJob(final Context mContext, int selectNum, int postedNum) {
 
         View view = View.inflate(mContext, R.layout.dialog_isapplyjob,
                 null);
 
         TextView tv_isapplyjob_resumename = (TextView) view.findViewById(R.id.tv_isapplyjob_resumename);
-        if(postedNum != 0){
-
-            tv_isapplyjob_resumename.setText("您投递" + selectNum + "个职位，其中" + postedNum +"个已投递，一周内不能重复投递");
-        }else{
+        if (postedNum != 0) {
+            tv_isapplyjob_resumename.setText("您投递" + selectNum + "个职位，其中" + postedNum + "个已投递，一周内不能重复投递");
+        } else {
             tv_isapplyjob_resumename.setText("您投递" + selectNum + "个职位");
         }
         final ImageView iv_cancel = (ImageView) view.findViewById(R.id.iv_isapplyjob_cancel);
@@ -54,7 +51,7 @@ public class JobApplyDialogUtil {
             }
         });
 
-        isApplyDialog = new MyDialog(mContext,view,R.style.mydialog_style);
+        isApplyDialog = new MyDialog(mContext, view, R.style.mydialog_style);
         isApplyDialog.show();
     }
 }
