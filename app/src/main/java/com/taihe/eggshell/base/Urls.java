084bd8629ee2abb820f67eaf58c823832c00350f
qq313915186 <qia7@qia7.com>
2015-09-07 09:30:39 +0800

    接口修改
    接口修改

--- app/src/main/java/com/taihe/eggshell/base/Urls.java
+++ app/src/main/java/com/taihe/eggshell/base/Urls.java
@@ -18,14 +18,14 @@ public class Urls {
 
 	/*********************************************************************/
 
-    public static final String METHOD_LOGIN = "http://195.198.1.211/eggker/interface/login";//登录
-    public static final String METHOD_REGIST = "http://195.198.1.211/eggker/interface/register";//注册
-    public static final String METHOD_REGIST_GETCODE = "http://195.198.1.211/eggker/interface/register/chTelphone";//注册获取验证码
+    public static final String METHOD_LOGIN = BASE_HYR_MOBILE_URL+"/login";//登录
+    public static final String METHOD_REGIST = BASE_HYR_MOBILE_URL+"/register";//注册
+    public static final String METHOD_REGIST_GETCODE = BASE_HYR_MOBILE_URL+"/register/chTelphone";//注册获取验证码
 
 
-    public static final String METHOD_BASIC = "http://195.198.1.211/eggker/interface/basicdata";//基本资料
-    public static final String METHOD_BASIC_SAVE = "http://195.198.1.211/eggker/interface/basicdata/add_basicdata";//保存修改的基本资料
-    public static final String METHOD_UPLOAD_IMAGE = "http://195.198.1.211/eggker/interface/basicdata/head";//上传头像
+    public static final String METHOD_BASIC = BASE_HYR_MOBILE_URL+"/basicdata";//基本资料
+    public static final String METHOD_BASIC_SAVE = BASE_HYR_MOBILE_URL+"/basicdata/add_basicdata";//保存修改的基本资料
+    public static final String METHOD_UPLOAD_IMAGE = BASE_HYR_MOBILE_URL+"/basicdata/head";//上传头像
 
 
     public static final String METHOD_GET_CODE = "/login/send_code";//获取验证码
@@ -33,14 +33,14 @@ public class Urls {
     public static final String METHOD_CHECK_CODE = "/login/next";//判断验证码
 
 
-    public static final String METHOD_JOB_LIST = "http://195.198.1.84/eggker/interface/Position/nearbycompany";//职位列表
-    public static final String METHOD_JOB_LIST_COLLECT = "http://195.198.1.84/eggker/interface/Position/collectlist";//收藏职位列表
-    public static final String METHOD_JOB_LIST_POST = "http://195.198.1.84/eggker/interface/Position/getPositionlist";//申请职位列表
-    public static final String METHOD_JOB_LIST_COLLECT_DELETE = "http://195.198.1.84/eggker/interface/Position/delcollectlist";//删除收藏职位
-    public static final String METHOD_JOB_LIST_POST_DELETE = "http://195.198.1.84/eggker/interface/Position/delgetPosition";//删除申请职位
-    public static final String METHOD_JOB_DETAIL = "http://195.198.1.84/eggker/interface/Position/details";//职位详情
-    public static final String METHOD_JOB_COLLECT = "http://195.198.1.84/eggker/interface/Position/collect";//职位收藏
-    public static final String METHOD_JOB_POST = "http://195.198.1.84/eggker/interface/Position/getPosition";//申请职位
+    public static final String METHOD_JOB_LIST = BASE_HYR_MOBILE_URL+"/Position/nearbycompany";//职位列表
+    public static final String METHOD_JOB_LIST_COLLECT = BASE_HYR_MOBILE_URL+"/Position/collectlist";//收藏职位列表
+    public static final String METHOD_JOB_LIST_POST = BASE_HYR_MOBILE_URL+"/Position/getPositionlist";//申请职位列表
+    public static final String METHOD_JOB_LIST_COLLECT_DELETE = BASE_HYR_MOBILE_URL+"/Position/delcollectlist";//删除收藏职位
+    public static final String METHOD_JOB_LIST_POST_DELETE = BASE_HYR_MOBILE_URL+"/Position/delgetPosition";//删除申请职位
+    public static final String METHOD_JOB_DETAIL = BASE_HYR_MOBILE_URL+"/Position/details";//职位详情
+    public static final String METHOD_JOB_COLLECT = BASE_HYR_MOBILE_URL+"/Position/collect";//职位收藏
+    public static final String METHOD_JOB_POST = BASE_HYR_MOBILE_URL+"/Position/getPosition";//申请职位
 
 
     public static final String METHOD_STATIC_DATA = "/Except/except_select";//静态数据
@@ -51,16 +51,16 @@ public class Urls {
     public static final String METHOD_DELETE_RESUME = "/Except/resume_del";//删除简历
     public static final String METHOD_RESUME_SCAN = "/Except/resume_preview";//简历预览
 
-    public static final String NEARBY_URL = "http://195.198.1.84/eggker/interface/Activity/index?";//玩出范
-    public static final String RESUME_WORK_URL = "http://195.198.1.120/eggker/interface/Except/resume_work";//工作经历
-    public static final String RESUME_EDU_URL = "http://195.198.1.120/eggker/interface/Except/resume_edu";//教育经历
-    public static final String RESUME_TRAIN_URL = "http://195.198.1.120/eggker/interface/Except/resume_training";//培训预览
-    public static final String RESUME_PROJECT_URL = "http://195.198.1.120/eggker/interface/Except/resume_project";//项目经验
-    public static final String RESUME_BOOK_URL = "http://195.198.1.120/eggker/interface/Except/resume_cert";//证书
-    public static final String RESUME_OTHER_URL = "http://195.198.1.120/eggker/interface/Except/resume_other";//自我评价
-    public static final String VIDEO_LIST_URL = "http://195.198.1.211/eggker/phpv/api.php/video/getPageList";//公开课
-    public static final String MEETING_LIST_URL = "http://195.198.1.84/eggker/interface/Infos/index";//信息台
-    public static final String COMPY_LIST_URL = "http://195.198.1.120/eggker/interface/Position/recommend_company";//名企推荐
-    public static final String COMPY_DETAIL_URL = "http://195.198.1.211/eggker/interface/Position/company_detail";//名企详情
+    public static final String NEARBY_URL = BASE_HYR_MOBILE_URL+"/Activity/index?";//玩出范
+    public static final String RESUME_WORK_URL = BASE_HYR_MOBILE_URL+"/Except/resume_work";//工作经历
+    public static final String RESUME_EDU_URL = BASE_HYR_MOBILE_URL+"/Except/resume_edu";//教育经历
+    public static final String RESUME_TRAIN_URL = BASE_HYR_MOBILE_URL+"/Except/resume_training";//培训预览
+    public static final String RESUME_PROJECT_URL = BASE_HYR_MOBILE_URL+"/Except/resume_project";//项目经验
+    public static final String RESUME_BOOK_URL = BASE_HYR_MOBILE_URL+"/Except/resume_cert";//证书
+    public static final String RESUME_OTHER_URL = BASE_HYR_MOBILE_URL+"/Except/resume_other";//自我评价
+    public static final String VIDEO_LIST_URL = "http://195.198.1.120/eggker/phpv/api.php/video/getPageList";//公开课
+    public static final String MEETING_LIST_URL = BASE_HYR_MOBILE_URL+"/Infos/index";//信息台
+    public static final String COMPY_LIST_URL = BASE_HYR_MOBILE_URL+"/Position/recommend_company";//名企推荐
+    public static final String COMPY_DETAIL_URL = BASE_HYR_MOBILE_URL+"/Position/company_detail";//名企详情
 
 }
