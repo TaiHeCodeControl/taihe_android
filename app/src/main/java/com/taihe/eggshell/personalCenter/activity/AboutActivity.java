a10d7ca12a7d4d0c261e8865cb4082f685a74656
qq313915186 <qia7@qia7.com>
2015-09-08 18:21:34 +0800

    优化
    优化

--- app/src/main/java/com/taihe/eggshell/personalCenter/activity/AboutActivity.java
+++ app/src/main/java/com/taihe/eggshell/personalCenter/activity/AboutActivity.java
@@ -1,6 +1,10 @@
 package com.taihe.eggshell.personalCenter.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.taihe.eggshell.R;
 import com.taihe.eggshell.base.BaseActivity;
@@ -11,11 +15,20 @@ import com.taihe.eggshell.base.BaseActivity;
 public class AboutActivity extends BaseActivity{
 
     private static final String TAG = "AboutActivity";
-
+    private TextView txt_about_tell;
     @Override
     public void initView() {
         setContentView(R.layout.activity_about);
+        txt_about_tell = (TextView)findViewById(R.id.txt_about_tell);
         super.initView();
+        txt_about_tell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:4000084111" ));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
